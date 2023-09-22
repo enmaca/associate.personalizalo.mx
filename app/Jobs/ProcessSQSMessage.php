2637d7ef835da700b28efcff08158ecd479858dc
Enrique Espiridion Martinez Cardenas <enmaca@hotmail.com>
2023-09-22 13:34:09 -0600

    ...

---
+++ app/Jobs/ProcessSQSMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessSQSMessage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function handle()
+    {
+        // Process the SQS message here
+        \Log::info('Received message from SQS', ['message' => $this->message]);
+    }
+}
