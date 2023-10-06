d9c285de65de26ca043cb0b154e623c6724346c3
Juan Víctor Jiménez Corzo <corzo@outlook.com>
2023-10-06 07:34:53 +0000

    oct/6 progress

--- resources/js/workshop/customizer/index.js
+++ resources/js/workshop/customizer/index.js
@@ -1,13 +1,32 @@
+import Alpine from "alpinejs";
 import Konva from "konva";
 
-const loadImg = (url) => {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = reject;
-    img.src = url;
-  });
-};
+Alpine.store("customizationData", {
+  selected: null,
+  items: [],
+
+  setItems(items) {
+    this.items.splice(0, this.items.length, ...items);
+  },
+
+  setSelected(value) {
+    document.querySelector('#modeSelect').checked = true;
+    this.selected = value;
+    tr.nodes(value === null ? [] : [mainLayer.findOne(`#${value}`)]);
+  },
+
+  remove(itemId) {
+    const index = this.items.findIndex((item) => item.id === itemId);
+
+    if (index === -1) {
+      return;
+    }
+    this.items.splice(index, 1);
+    customizationData.remove(index);
+  },
+});
+
+Alpine.start();
 
 let sceneWidth = 0;
 let sceneHeight = 0;
@@ -16,57 +35,37 @@ const stage = new Konva.Stage({
   container: "customizator",
 });
 
-const staticLayer = new Konva.Layer();
-stage.add(staticLayer);
-
-const setBackground = (url) => {
-  loadImg(url).then((img) => {
-    sceneWidth = img.naturalWidth;
-    sceneHeight = img.naturalHeight;
-
-    stage.width(sceneWidth);
-    stage.height(sceneHeight);
-
-    const bg = new Konva.Image({
-      x: 0,
-      y: 0,
-      image: img,
-      width: sceneWidth,
-      height: sceneHeight,
-      listening: false,
-    });
-    staticLayer.add(bg);
-
-    fitStageIntoParentContainer();
-  });
-};
-
 document.querySelector("#bgSelector").addEventListener("change", (e) => {
   const file = e.target.files[0];
   const reader = new FileReader();
   reader.onload = (ev) => {
-    setBackground(ev.target.result);
+    // setBackground(ev.target.result);
   };
   reader.readAsDataURL(file);
 });
 
-setBackground(
-  "https://loremflickr.com/cache/resized/65535_52945682677_568fc25818_c_600_800_nofilter.jpg"
-);
+const hasScroll = () => document.body.scrollHeight > document.body.clientHeight;
+
+let oldhasScroll = hasScroll();
 
 const fitStageIntoParentContainer = () => {
-  var container = document.querySelector("#customizator");
+  const container = document.querySelector("#customizator");
 
   // now we need to fit stage into parent container
-  var containerWidth = container.offsetWidth;
+  const containerWidth = container.offsetWidth;
 
   // but we also make the full scene visible
   // so we need to scale all objects on canvas
-  var scale = containerWidth / sceneWidth;
+  const scale = containerWidth / sceneWidth;
 
   stage.width(sceneWidth * scale);
   stage.height(sceneHeight * scale);
   stage.scale({ x: scale, y: scale });
+
+  if (oldhasScroll !== hasScroll()) {
+    oldhasScroll = hasScroll();
+    fitStageIntoParentContainer();
+  }
 };
 
 let debounceTimeout;
@@ -82,8 +81,21 @@ const getToolMode = () => {
   return document.querySelector('input[name="tool-mode"]:checked').value;
 };
 
+const mainLayer = new Konva.Layer();
+stage.add(mainLayer);
+
 const selectorLayer = new Konva.Layer();
 stage.add(selectorLayer);
+
+const tr = new Konva.Transformer({
+  ignoreStroke: true,
+  anchorStroke: "rgba(0,0,255,1)",
+  anchorFill: "rgba(0,0,255,0.5)",
+  anchorStyleFunc: (anchor) => {
+    anchor.cornerRadius(10);
+  },
+});
+selectorLayer.add(tr);
 
 const selectionRectangle = new Konva.Rect({
   fill: "rgba(0,0,255,0.5)",
@@ -126,9 +138,6 @@ stage.on("mousemove touchmove", (e) => {
   });
 });
 
-const mainLayer = new Konva.Layer();
-stage.add(mainLayer);
-
 stage.on("mouseup touchend", (e) => {
   // do nothing if we didn't start selection
   if (!selectionRectangle.visible()) {
@@ -137,7 +146,11 @@ stage.on("mouseup touchend", (e) => {
   e.evt.preventDefault();
   // update visibility in timeout, so we can check it in click event
   setTimeout(() => {
-    const generatedRect = new Konva.Rect({
+    const thW = stage.width() / 100 * 5;
+    const thH = stage.height() / 100 * 5;
+
+    const newRect = {
+      type: "rect",
       x: selectionRectangle.x(),
       y: selectionRectangle.y(),
       width: selectionRectangle.width(),
@@ -145,10 +158,154 @@ stage.on("mouseup touchend", (e) => {
       stroke: "red",
       strokeWidth: 1,
       dash: [10, 5],
-    });
+      strokeScaleEnabled: false,
+    };
 
-    mainLayer.add(generatedRect);
+    if (newRect.width === 0 && newRect.height === 0) {
+      document.querySelector('#modeSelect').checked = true;
+    } else if (newRect.width > thW && newRect.height > thH) {
+      customizationData.add(newRect);
+    } else {
+      setTimeout(() => {
+        alert('too small, adjust the threshold');
+      }, 200);
+    }
 
     selectionRectangle.visible(false);
   });
 });
+
+stage.on("click tap", function (e) {
+  Alpine.store("customizationData").setSelected(null);
+
+  if (tr.nodes()?.[0]) {
+    tr.nodes()[0].draggable(false);
+  }
+
+  if (getToolMode() === "add") {
+    return;
+  }
+
+  // if click on empty area - remove all selections
+  if (e.target === stage) {
+    tr.nodes([]);
+    return;
+  }
+
+  if (!e.target.id()) {
+    tr.nodes([]);
+    return;
+  }
+
+  tr.nodes([e.target]);
+  e.target.draggable(true);
+  Alpine.store("customizationData").setSelected(e.target.id());
+});
+
+const transformHandler = (e) => {
+  const node = e.target;
+  const scaleX = node.scaleX();
+  const scaleY = node.scaleY();
+
+  // we will reset it back
+  node.scaleX(1);
+  node.scaleY(1);
+
+  const width = node.width() * scaleX;
+  const height = node.height() * scaleY;
+
+  node.width(width);
+  node.height(height);
+};
+
+const customizationData = {
+  selected: null,
+  items: [],
+  async renderItem(item) {
+    item.id = "item_" + (item.modelId || Date.now());
+    if (item.type === "imageFromUrl") {
+      return new Promise((resolve) => {
+        Konva.Image.fromURL(item.src, (konvaObj) => {
+          const img = konvaObj.image();
+
+          const { naturalWidth, naturalHeight } = img;
+
+          konvaObj.setAttrs({
+            ...item,
+            width: naturalWidth,
+            height: naturalHeight,
+          });
+
+          konvaObj.on("transform", transformHandler);
+
+          mainLayer.add(konvaObj);
+
+          sceneWidth = naturalWidth;
+          sceneHeight = naturalHeight;
+
+          resolve();
+        });
+      });
+    } else if (item.type === "rect") {
+      const konvaObj = new Konva.Rect(item);
+
+      konvaObj.on("transform", transformHandler);
+
+      mainLayer.add(konvaObj);
+    }
+  },
+  async render() {
+    for (const item of this.items) {
+      await this.renderItem(item);
+    }
+  },
+  async set(items) {
+    this.items = items;
+    await this.render();
+
+    Alpine.store("customizationData").setItems(this.items);
+
+    fitStageIntoParentContainer();
+  },
+  add(item) {
+    customizationData.items.push(item);
+    this.renderItem(item);
+
+    Alpine.store("customizationData").setItems(this.items);
+  },
+  remove(index) {
+    customizationData.items.splice(index, 1);
+    mainLayer.children[index].destroy();
+    tr.nodes([]);
+  },
+  update(index, data) {
+    customizationData.items[index] = data;
+  },
+  setSelected(value) {
+    this.selected = value;
+  },
+};
+
+customizationData.set([
+  {
+    modelId: 1,
+    type: "imageFromUrl",
+    x: 0,
+    y: 0,
+    src: "https://loremflickr.com/cache/resized/8032_29732634126_16ac267f9c_b_800_600_nofilter.jpg",
+    width: 0,
+    height: 0,
+    listening: false,
+  },
+  {
+    modelId: 2,
+    height: 227.27272727272728,
+    dash: [10, 5],
+    stroke: "red",
+    strokeWidth: 1,
+    type: "rect",
+    width: 245.4545454545454,
+    x: 172.72727272727272,
+    y: 136.93181818181816,
+  },
+]);
--- resources/views/workshop/customizer/index.blade.php
+++ resources/views/workshop/customizer/index.blade.php
@@ -1,6 +1,6 @@
 @extends('workshop.master')
 
-@section('title', 'Fuentes')
+@section('title', 'Personalizador')
 
 @vite('resources/js/workshop/customizer/index.js')
 
@@ -25,14 +25,49 @@
             
                 <input type="radio" class="btn-check" name="tool-mode" id="modeSelect" autocomplete="off" value="select">
                 <label class="btn btn-outline-primary d-flex align-items-center" for="modeSelect" data-bs-toggle="tooltip" data-bs-title="Modo selección">
-                    <i class="ph ph-selection"></i>
+                    <i class="ph ph-hand-pointing"></i>
                 </label>
             </div>
         </div>
         <div id="customizator"></div>
     </div>
     <div class="col-7">
-        Menu
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active d-flex align-items-center" id="layerTab" data-bs-toggle="tab" data-bs-target="#layerTabPane" type="button" role="tab" aria-controls="layerTabPane" aria-selected="true">
+                    <i class="ph ph-stack"></i>
+                    <span class="ms-2">Layers</span>
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link d-flex align-items-center" id="settings-tab" data-bs-toggle="tab" data-bs-target="#settingsTabPane" type="button" role="tab" aria-controls="settingsTabPane" aria-selected="false">
+                    <i class="ph ph-gear"></i>
+                    <span class="ms-2">Configuración</span>
+                </button>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div x-data class="tab-pane fade show active" id="layerTabPane" role="tab-panel" aria-labelledby="layerTab" tabindex="0">
+                <ul class="list-group">
+                    <template x-for="(item, index) in $store.customizationData.items" :key="item.id">
+                        <template x-if="index > 0">
+                            <li href="#" class="list-group-item d-flex justify-content-between align-items-center" :class="$store.customizationData.selected === item.id ? 'active' : ''">
+                                <button class="btn btn-sm btn-info" x-on:click="$store.customizationData.setSelected(item.id)">
+                                    <i class="ph ph-hand-pointing"></i>
+                                </button>
+                                <div class="flex-grow-1 ms-2" x-text="item.id"></div>
+                                <button class="btn btn-sm btn-danger" x-on:click="$store.customizationData.remove(item.id)">
+                                    <i class="ph ph-trash"></i>
+                                </button>
+                            </li>
+                        </template>
+                    </template>
+                </ul>
+            </div>
+            <div class="tab-pane fade" id="settingsTabPane" role="tab-panel" aria-labelledby="settings-tab" tabindex="0">
+                settings
+            </div>
+        </div>
     </div>
 </div>
 @endsection
