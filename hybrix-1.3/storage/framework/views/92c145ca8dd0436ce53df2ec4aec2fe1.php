7aa3179313e88ded3484dc17fa528ad602adcfbb
Enrique Espiridion Martinez Cardenas <enmaca@hotmail.com>
2023-10-11 11:21:54 -0600

    Fix Directories

--- hybrix-1.3/storage/framework/views/92c145ca8dd0436ce53df2ec4aec2fe1.php
+++
@@ -1,7 +0,0 @@
-<!-- JAVASCRIPT -->
-<script src="<?php echo e(URL::asset('build/libs/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/libs/simplebar/simplebar.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/plugins/lord-icon-2.1.0.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/plugins.js')); ?>"></script>
-
-<?php echo $__env->yieldContent('script'); ?><?php /**PATH /Users/enriquemartinez/php-projects/hybrix-1.3/resources/views/layouts/vendor-scripts.blade.php ENDPATH**/ ?>
