fa672a618b328cdbe21e7fab058090548f4dc129
Enrique Martinez <enmaca@hotmail.com>
2023-11-12 20:07:20 -0600

    12 Noviembre 23 Depto 3 Refactoring code, y CleanUP

--- resources/toDelete/views/livewire/order/create/products-table.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    {!! $orderId !!}
-</div>
--- resources/toDelete/views/workshop/head-css.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Bootstrap Css -->
-@vite('resources/scss/bootstrap.scss')
-<!-- Icons Css -->
-@vite('resources/scss/icons.scss')
-<!-- App Css-->
-@vite('resources/scss/app.scss')
-<!-- custom Css-->
-@vite('resources/scss/custom.scss')
