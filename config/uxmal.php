dc250fbb6d48d1c92203b76c134e51efaeb0cf73
armandohg <armandohg@gmail.com>
2023-11-24 11:34:50 -0600

    Generar link con base al IDE configurado.

--- config/uxmal.php
+++ config/uxmal.php
@@ -2,4 +2,10 @@
 
 return [
     'debug' => env('UXMAL_DEBUG', false),
+    'ide' => env('UXMAL_IDE', 'phpstorm'),
+
+    'ide_formats' => [
+        'phpstorm' => 'phpstorm://open?file=%s&line=%d',
+        'vscode' => 'vscode://file/%s:%d',
+    ]
 ];
